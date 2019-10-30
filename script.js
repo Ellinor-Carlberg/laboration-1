539d7a8731d884e3fd5730cb0eb1444e9c6b0ba2
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2019-10-30 10:50:10 +0100

    first Question in js done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function startGame() {
-    const allowedActionsWakeHuman = ["wake", "human", "wake human"]
-    const allowedActionsGoToKitchen = ["wake", "human", "wake human"]
-    const isQuestionAnsweredCorrectly = false
+    const allowedActionsWakeHuman = ["wake", "human", "wake human", "wake the human", "wake human up", "wake the human up"]
+    const allowedActionsGoToKitchen = ["kitchen", "the kitchen", "to the kitchen" , "go to the kitchen"]
+    const isQuestionAnsweredCorrectlyFirstQuestion = false
     
-    while (!isQuestionAnsweredCorrectly) {
+    while (!isQuestionAnsweredCorrectlyFirstQuestion) {
         
         const firstQuestion = prompt("You are the cute dog Inka and you want to go out and play. You are in the bedroom, one human is sleeping and the other human is in the kitchen. Du you want to go try to wake up the human that is sleeping or do you want to go to the kitchen?");
         for (const action of allowedActionsWakeHuman) {
@@ -15,41 +15,40 @@ function startGame() {
                 // går att anropa en annan funktion/gå till en annan funktion här
             }
         }
-
-    }
-    
-    
-    if (firstQuestion.toLowerCase() === "wake") {
-    alert ("Human: zzZZZzzzzz zzzZZZzzzz. You try to wake up the human, it doesnt work. You go out in the kitchen...");
-}
-    else if  (firstQuestion === "Wake") {
-        alert ("Human: zzZZZzzzzz zzzZZZzzzz. You try to wake up the human, it doesnt work. You go out in the kitchen...");
-    }
-    else if  (firstQuestion === "human") {
-        alert ("Human: zzZZZzzzzz zzzZZZzzzz. You try to wake up the human, it doesnt work. You go out in the kitchen...");
-    }
-    else if  (firstQuestion === "kitchen") {
-        alert ("You go out in the kitchen...");
-    }
-    else if  (firstQuestion === "go kitchen") {
-        alert ("You go out in the kitchen...");
-    }
-    else if  (firstQuestion === "go to kitchen") {
-        alert ("You go out in the kitchen...");
-    }
-    
-    //else -få frågan igen
-    
-    secondQuestion()
+        for (const action of allowedActionsGoToKitchen) {
+            if (action === firstQuestion.toLowerCase()) {
+                alert ("You go out in the kitchen...");
+                isQuestionAnsweredCorrectly = true;
+                break; 
+            }
+        }
+secondQuestion()
 }
 
 function secondQuestion() {
-    const secondQuestion = prompt("Now you are in the kitchen. There is a human eating food and a cat. Do you want to go to the human or do you want to go to the cat?")
-    {
-        if (secondQuestion === "human") 
-        alert("Human: Du you want to do a trick?");
+    const allowedActionsTalkToEatingHuman = [ "human", "the human", "to the human", "go to the human"]
+    const allowedActionsTalkToCat = ["cat", "the cat", "to the cat" , "go to the cat"]
+    const isQuestionAnsweredCorrectlySecondQuestion = false
+    
+    while (!isQuestionAnsweredCorrectlySecondQuestion) {
         
-        else if  (secondQuestion === "cat")
-          alert ("Cat: Hi Inka. You want to go outside? I will help you! Lets go to the sleeping human, I can wake the human for you.");
+        const secondQuestion = prompt("Now you are in the kitchen. There is a human eating food and a cat. Do you want to go to the human or do you want to go to the cat?");
+        for (const action of allowedActionsTalkToEatingHuman) {
+            if (action === secondQuestion.toLowerCase()) {
+                alert ("Human: Du you want to do a trick?");
+                isQuestionAnsweredCorrectlyTwo = true;
+                break;
+                // går att anropa en annan funktion/gå till en annan funktion här
+            }
+        }
+        for (const action of allowedActionsTalkToCat) {
+            if (action === secondQuestion.toLowerCase()) {
+                alert ("Cat: Hi Inka. You want to go outside? I will help you! Lets go to the sleeping human, I can wake the human for you.");
+                isQuestionAnsweredCorrectlyTwo = true;
+                break;
+                // går att anropa en annan funktion/gå till en annan funktion här
+            }
+        }
     }
+}
 }
