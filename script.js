dafa791b0bfd15cf3ce5a1225ebe3b3988925cd1
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2019-10-30 12:26:46 +0100

    1-4 question done in js file

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 
 function startGame() {
-    const allowedActionsWakeHuman = ["wake", "human", "wake human", "wake the human", "wake human up", "wake the human up"]
+    const allowedActionsTalkToSleepingHuman = ["wake", "human", "wake human", "wake the human", "wake human up", "wake the human up"]
     const allowedActionsGoToKitchen = ["kitchen", "the kitchen", "to the kitchen" , "go to the kitchen"]
     const isQuestionAnsweredCorrectlyFirstQuestion = false
     
-    while (!isQuestionAnsweredCorrectlyFirstQuestion) {
-        
+    while (!isQuestionAnsweredCorrectlyFirstQuestion) {  
         const firstQuestion = prompt("You are the cute dog Inka and you want to go out and play. You are in the bedroom, one human is sleeping and the other human is in the kitchen. Du you want to go try to wake up the human that is sleeping or do you want to go to the kitchen?");
-        for (const action of allowedActionsWakeHuman) {
+        for (const action of allowedActionsTalkToSleepingHuman) {
             if (action === firstQuestion.toLowerCase()) {
                 alert ("Human: zzZZZzzzzz zzzZZZzzzz. You try to wake up the human, it doesnt work. You go out in the kitchen...");
-                isQuestionAnsweredCorrectly = true;
+                secondQuestion()
+                isQuestionAnsweredCorrectlyFirstQuestion = true; 
                 break;
-                // går att anropa en annan funktion/gå till en annan funktion här
             }
         }
         for (const action of allowedActionsGoToKitchen) {
             if (action === firstQuestion.toLowerCase()) {
                 alert ("You go out in the kitchen...");
-                isQuestionAnsweredCorrectly = true;
-                break; 
+                secondQuestion()
+                isQuestionAnsweredCorrectlyFirstQuestion = true; 
+                break;
             }
         }
-secondQuestion()
+    }
 }
 
 function secondQuestion() {
@@ -35,20 +35,72 @@ function secondQuestion() {
         const secondQuestion = prompt("Now you are in the kitchen. There is a human eating food and a cat. Do you want to go to the human or do you want to go to the cat?");
         for (const action of allowedActionsTalkToEatingHuman) {
             if (action === secondQuestion.toLowerCase()) {
-                alert ("Human: Du you want to do a trick?");
-                isQuestionAnsweredCorrectlyTwo = true;
+                thirdQuestion()
+                isQuestionAnsweredCorrectlySecondQuestion = true;
                 break;
-                // går att anropa en annan funktion/gå till en annan funktion här
             }
         }
         for (const action of allowedActionsTalkToCat) {
             if (action === secondQuestion.toLowerCase()) {
                 alert ("Cat: Hi Inka. You want to go outside? I will help you! Lets go to the sleeping human, I can wake the human for you.");
-                isQuestionAnsweredCorrectlyTwo = true;
+                isQuestionAnsweredCorrectlySecondQuestion = true;
                 break;
-                // går att anropa en annan funktion/gå till en annan funktion här
             }
         }
     }
 }
+
+function thirdQuestion() {
+    const allowedActionsDoATrick = [ "yes", "yes i do", "i do", "trick"]
+    const allowedActionsNotDoATrick = ["no", "no i dont", "no i dont want" , "no trick"]
+    const isQuestionAnsweredCorrectlyThirdQuestion = false
+    
+    while (!isQuestionAnsweredCorrectlyThirdQuestion) {
+        
+        const thirdQuestion = prompt("Hi Inka. Do you want to do a trick?");
+        for (const action of allowedActionsDoATrick) {
+            if (action === thirdQuestion.toLowerCase()) {
+                alert ("You did a trick and get a doggo candy!");
+                fourthQuestion()
+                isQuestionAnsweredCorrectlyThirdQuestion = true;
+                //vill att man ska kunna svara ja/nej, göra ett trick max 3 ggr, sen slut. 
+                break;
+            }
+        }
+        for (const action of allowedActionsNotDoATrick) {
+            if (action === thirdQuestion.toLowerCase()) {
+                alert ("You didnt do a trick, but you still a cute doggo.");
+                fourthQuestion()
+                isQuestionAnsweredCorrectlyThirdQuestion = true;
+                break;
+            }
+        }
+    }
 }
+
+function fourthQuestion() {
+    const allowedActionsDoAnotherTrick = [ "trick", "do trick", "do a trick"]
+    const allowedActionsTalkToCat = ["cat", "the cat", "to the cat" , "go to the cat"]
+    const isQuestionAnsweredCorrectlyFourthQuestion = false
+    
+    while (!isQuestionAnsweredCorrectlyFourthQuestion) {
+        
+        const fourthQuestion = prompt("Hmmmm.... What do you want to do now, do another trick or go to the cat?");
+        for (const action of allowedActionsDoAnotherTrick) {
+            if (action === fourthQuestion.toLowerCase()) {
+                alert ("You did another trick and get one more doggo candy!");
+                isQuestionAnsweredCorrectlyFourthQuestion = true;
+                //vill att man ska kunna svara ja/nej, göra ett trick max 3 ggr, sen slut. 
+                break;
+            }
+        }
+        for (const action of allowedActionsTalkToCat) {
+            if (action === fourthQuestion.toLowerCase()) {
+                alert ("You go to the cat.");
+                isQuestionAnsweredCorrectlyFourthQuestion = true;
+                break;
+            }
+        }
+    }
+}
+
